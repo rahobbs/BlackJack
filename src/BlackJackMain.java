65206e03c1e6b019f7cbea62386ff6c11e443bc8
Rachael Hobbs <hobbs.rachael@gmail.com>
2015-07-21 17:40:31 -0400

    added dealer logic, let player decide to hit or stay

--- src/BlackJackMain.java
+++ src/BlackJackMain.java
@@ -28,11 +28,23 @@ public class BlackJackMain {
             Card playerCard = new Card();
             Card dealerCard = new Card();
             playerOneTotal = playerOneTotal + playerCard.value;
-            dealerTotal = dealerTotal + dealerCard.value;
-
             System.out.println("You get a " + playerCard.value
                     + ". Your total is " + playerOneTotal + ".");
+            
+            if (Score.isGameOver(playerOneTotal, dealerTotal) ==true){
+                break;
+            }
+            
+            if (dealerTotal < 16){
+            dealerTotal = dealerTotal + dealerCard.value;
             System.out.println("The dealer got a " + dealerCard.value + ".");
+            } else{
+                System.out.println("The dealer has chosen to stay.");
+            }
+
+                        
+            System.out.println("Would you like to \"hit\" or \"stay?\"");
+            String action = keyboard.next();
         }
 
         System.out.println("Game over. "
