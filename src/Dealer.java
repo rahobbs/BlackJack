61f52bb69935c4b5e432543153c2902b6ae50641
Rachael Hobbs <hobbs.rachael@gmail.com>
2015-07-28 13:38:50 -0400

    fixes to Dealer.java

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,24 +1,25 @@
-public class Dealer extends Player{
+public class Dealer extends Player {
     
+    String userInput = "";
+
     public Choice dealerChoice(int playerTotal) {
-        
-        String userInput = "";
-        
-        while (true) {
-            if (this.playerTotal > 16){
-                userInput = "STAY";
-            } else{
-               userInput = "HIT";
-            }
-            switch (userInput) {
-            case "HIT":
-                return Choice.HIT;
-            case "STAY":
-                return Choice.STAY;
-            default:
-                System.out.print("That option isn't available. ");
-                continue;
-            }
+
+
+
+        if (this.playerTotal > 16) {
+            userInput = "STAY";
+        } else {
+            userInput = "HIT";
+        }
+        switch (userInput) {
+        case "HIT":
+            return Choice.HIT;
+        case "STAY":
+            return Choice.STAY;
+        default:
+            System.out.print("That option isn't available. ");
+            return Choice.NONE;
         }
     }
+
 }
