8e48f77c0243f66cb6131ba2c841b3e4d5577f18
Rachael Hobbs <hobbs.rachael@gmail.com>
2015-08-11 10:59:40 -0400

    adding 'stay' test case to DealerTest.java

--- src/DealerTest.java
+++ src/DealerTest.java
@@ -1,4 +1,3 @@
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -6,12 +5,23 @@ import org.junit.Test;
 public class DealerTest {
 
     @Test
-    public void testDealerLogic() {
+    public void testDealerHit() {
         Player dealer = new Dealer();
         for(int i = 1; i < 17; i++){
             dealer.playerTotal = i;
             Player.Choice choice1 = dealer.makeChoice();
             assertEquals(Player.Choice.HIT, choice1);
+        }
+    }
+    
+    @Test
+    public void testDealerStay() {
+        Player dealer = new Dealer();
+        
+        for(int i = 17; i < 22; i++){
+            dealer.playerTotal = i;
+            Player.Choice choice1 = dealer.makeChoice();
+            assertEquals(Player.Choice.STAY, choice1);
         }
     }
 }
