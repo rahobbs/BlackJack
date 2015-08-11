b6b95943689a3a831cb26f3586bcf76dd37e0215
Rachael Hobbs <hobbs.rachael@gmail.com>
2015-08-11 11:17:05 -0400

    setting up new test cases

---
+++ src/PlayerTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void testMakeChoice(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testApplyChoice() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/ScoreTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ScoreTest {
+
+    @Test
+    public void testIsGameOver() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testPickWinner() {
+        fail("Not yet implemented");
+    }
+
+}
