07b1bb677468a202f683cd8f9e6b60dd5d7c76f1
Rachael Hobbs <hobbs.rachael@gmail.com>
2015-08-13 16:43:45 -0400

    writing new tests

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -12,13 +12,20 @@ public class PlayerTest {
     }
 
     @Test
-    public void testApplyChoice() {
-        Random rand = new Random();
-        int playerTotal = 0;
-        int mCard = rand.nextInt(11 - 2 + 1) + 2;
-        
-        
-        
+    public void testIsHit() {
+        Player player = new Player();
+        assertEquals(player.isHit(Player.Choice.HIT), true);
+        assertEquals(player.isHit(Player.Choice.STAY), false);      
+    }
+    
+    @Test
+    public void testDrawCard(){
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testNewTotal(){
+        fail("Not yet implemented");
     }
 
 }
