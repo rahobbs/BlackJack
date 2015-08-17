ca11c4148c4b074538360b095bf034ab61fd6731
Rachael Hobbs <hobbs.rachael@gmail.com>
2015-08-17 19:55:49 -0400

    moved game logic to Game.java

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,58 +1,15 @@
-import java.util.Random;
-
 public class BlackJack {
 
     public static void main(String[] args) {
 
-        Random rand = new Random();
-        Player playerOne = new Player();
-        Player dealer = new Dealer();
-        Player.Choice playerOneChoice;
-        Player.Choice dealerChoice;
-        System.out.println("Welcome to Blackjack!");
-        System.out.println("");
-
-        // initial draw, create playerOne, dealer, initial cards and total
-
-        int playerCardOne = rand.nextInt(11 - 2 + 1) + 2;
-        int playerCardTwo = rand.nextInt(11 - 2 + 1) + 2;
-        playerOne.playerTotal = playerCardOne + playerCardTwo;
-
-        int dealerCardOne = rand.nextInt(11 - 2 + 1) + 2;
-        int dealerCardTwo = rand.nextInt(11 - 2 + 1) + 2;
-        dealer.playerTotal = dealerCardOne + dealerCardTwo;
-
-        System.out.println("You get a " + playerCardOne + " and a "
-                + playerCardTwo + ". Your total is " + playerOne.playerTotal
-                + ".");
-        System.out.println("The dealer got a " + dealerCardOne
-                + ". His second card and total are hidden.");
-
-        // gameplay
-        do {
-            playerOneChoice = playerOne.makeChoice();
-            dealerChoice = dealer.makeChoice();
-
-            // player one's turn
-
-            playerOne.newTotal(playerOne.drawCard(playerOne
-                    .isHit(playerOneChoice)));
-            System.out.println("Your total is " + playerOne.playerTotal + ".");
-            // check to see if player one's action ended the game
-            Score.isGameOver(playerOne.playerTotal, dealer.playerTotal,
-                    playerOneChoice, dealerChoice);
-            // dealer's turn
-            System.out.println("The dealer chose to " + dealerChoice.name()
-                    + ". ");
-            dealer.newTotal(dealer.drawCard(dealer.isHit(dealerChoice)));
-
-        } while (Score.isGameOver(playerOne.playerTotal, dealer.playerTotal,
-                playerOneChoice, dealerChoice) == false);
+        Game.newGame();
+        Game.initialDraw();
+        Game.playGame();
 
         // declare winner when game is over
         System.out.print(Score.reportWinner(Score.pickWinner(
-                playerOne.playerTotal, dealer.playerTotal)));
-        Score.finalScore(playerOne.playerTotal, dealer.playerTotal);
+                Game.playerOne.playerTotal, Game.dealer.playerTotal)));
+        Score.finalScore(Game.playerOne.playerTotal, Game.dealer.playerTotal);
     }
 
 }
--- src/Game.java
+++ src/Game.java
@@ -1,21 +1,20 @@
 import java.util.Random;
 
-
 public class Game {
-    
-    Random rand = new Random();
-    Player playerOne = new Player();
-    Player dealer = new Dealer();
-    Player.Choice playerOneChoice;
-    Player.Choice dealerChoice;
-    
-    public static void newGame(){
-        
+
+    static Random rand = new Random();
+    static Player playerOne = new Player();
+    static Player dealer = new Dealer();
+    static Player.Choice playerOneChoice;
+    static Player.Choice dealerChoice;
+
+    public static void newGame() {
+
         System.out.println("Welcome to Blackjack!");
         System.out.println("");
     }
-    
-    public static void initialDraw(){
+
+    public static void initialDraw() {
         int playerCardOne = rand.nextInt(11 - 2 + 1) + 2;
         int playerCardTwo = rand.nextInt(11 - 2 + 1) + 2;
         playerOne.playerTotal = playerCardOne + playerCardTwo;
@@ -29,5 +28,29 @@ public class Game {
                 + ".");
         System.out.println("The dealer got a " + dealerCardOne
                 + ". His second card and total are hidden.");
+    }
+
+    public static void playGame() {
+        do {
+            playerOneChoice = Game.playerOne.makeChoice();
+            dealerChoice = Game.dealer.makeChoice();
+
+            // player one's turn
+
+            Game.playerOne.newTotal(Game.playerOne.drawCard(Game.playerOne
+                    .isHit(playerOneChoice)));
+            System.out.println("Your total is " + Game.playerOne.playerTotal
+                    + ".");
+            // check to see if player one's action ended the game
+            Score.isGameOver(Game.playerOne.playerTotal,
+                    Game.dealer.playerTotal, playerOneChoice, dealerChoice);
+            // dealer's turn
+            System.out.println("The dealer chose to " + dealerChoice.name()
+                    + ". ");
+            Game.dealer.newTotal(Game.dealer.drawCard(Game.dealer
+                    .isHit(dealerChoice)));
+
+        } while (Score.isGameOver(Game.playerOne.playerTotal,
+                Game.dealer.playerTotal, playerOneChoice, dealerChoice) == false);
     }
 }
